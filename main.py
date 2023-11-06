0f6b47b9af55c91d5acf757b154266c6de075885
nigelkaganzi <nkaganzi@students.kennesaw.edu>
2023-11-06 18:22:57 -0500

    add_expense function corrections
    Fixed a couple mistakes in the function such as changing i to a string on line 24 as well as changing the first option to expense_name instead of just expense

--- main.py
+++ main.py
@@ -15,13 +15,13 @@ def add_expense():
     Function to add expense.
     '''
     print("Add Expense: ")
-    expense = input("Expense Name: ")
+    expense_name = input("Expense Name: ")
     amount  = float(input("Amount: "))
 
     # Print the categories and allow the user to choose
     print("Choose a category")
     for i, category in enumerate(categories,start=1):
-        print (i + ',' + category)
+        print (str(i) + ',' + category)
     try:
         category_choice = int(input("Enter the category number: "))
         category = category_choice - 1
@@ -35,7 +35,7 @@ def add_expense():
 
     # Dictionary to define expense 
     expense = {
-        "Name": name,
+        "Name": expense_name,
         "Amount": amount,
         "Category": category,
         "Date": date
@@ -43,3 +43,4 @@ def add_expense():
     expenses.append(expense)
     print("Expense added successfully!")
 
+add_expense()
