554ccf95783eea0c4629a558fbcc0262fe05470f
nigelkaganzi <nkaganzi@students.kennesaw.edu>
2023-11-08 16:37:03 -0500

    main function
    I created the main function and defined the rest of my functions but I have not created the code for them. I also created  a save to csv function

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import tkinter as tk
 from tkinter import ttk, messagebox, simpledialog
 import csv
 import matplotlib.pyplot as plt
+import os
 
 # Initializes an empty list to store expense data
 expenses = []
@@ -10,6 +11,7 @@ expenses = []
 categories = ["Food", "Entertainment", "Utilities", "Housing", "Shopping", "Transportation", "Other"]
 
 # Start by defining the menu options
+# First function is an add expense function for users to add their expenses.
 def add_expense():
     '''
     Function to add expense.
@@ -43,4 +45,74 @@ def add_expense():
     expenses.append(expense)
     print("Expense added successfully!")
 
-add_expense()
+# Second Function is to save  expenses to a CSV
+def save_to_csv():
+    csv_file = "expenses.csv"
+
+# Check if the CSV file exists and create a header row if it does
+    file_exists = False
+    try:
+        with open(csv_file,"r") as file:
+            if file.read(1):
+                file_exists = True
+    except FileNotFoundError:
+        pass
+    
+    with open(csv_file, "a", newline="") as file:
+        fieldnames = ["Name", "Amount", "Category", "Date"]
+        writer = csv.DictWriter(file, fieldnames=fieldnames)
+
+        if not file_exists:
+            writer.writeheader()
+
+        for expense in expenses:
+            writer.writerow(expense)
+def edit_expense():
+    #code for edit expense function
+    print("code not finished")
+def delete_expense(): 
+    #code for delete expense function
+    print("code not finished")
+def view_expense():
+    #code for view expense function
+    print("code not finished")
+def generate_report():
+    #code for generate report function
+    print("code not finished")
+def exit():
+    #code for exit function
+    print("code not finished")
+def main():
+    while True:    
+        ''' 
+        This is the main function to show to users 
+        '''
+        print("Welcome to the Expense tracker.")
+
+        print("""
+            Welcome to the Expense tracker.
+            [1] - Add Expense
+            [2] - Edit Expense
+            [3] - Delete Expense
+            [4] - View expenses
+            [5] - Generate Report
+            [6] - Exit
+            """)
+        
+        action = input("What would you like to do? ")
+        if action == "1":
+            add_expense()
+        elif action == "2":
+            edit_expense()
+        elif action == "3":
+            delete_expense()
+        elif action == "4":
+            view_expense
+        elif action == "5":
+            generate_report()
+        elif action == "6":
+            exit()
+        else:
+            print("Invalid option. Please try again")
+if __name__ == "__main__":
+    main()
